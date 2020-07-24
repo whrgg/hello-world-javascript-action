85cebedd6b6f3e39cdd94e8a72ec22896e5d677a
lizhiwen <lizhiwen@sprout-edu.com>
2020-07-24 18:05:29 +0800

    test

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
-  core.setFailed(error.message);
+  core.setFailed(error.message); // 报错
 }
