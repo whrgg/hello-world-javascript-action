d60bf9ac96712c13ef1fb1700f8e2ea98e3d9a0d
吴好人 <120736499+whrgg@users.noreply.github.com>
2024-09-22 21:09:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  // 获取 action.yml 中 input 类 定义的变量
+  // 获取当前工作目录
+  console.log(`Current working directory: ${process.cwd()}`);
+
+  // 获取 action.yml 中 input 类定义的变量
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
 
-  // 输出 action.yml 中 output 类 定义的变量
+  // 输出 action.yml 中 output 类定义的变量
   core.setOutput("time", time);
 
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  // 触发工作流完成钩子
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // 获取触发工作流的事件的 JSON 有效载荷
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message); // 报错
